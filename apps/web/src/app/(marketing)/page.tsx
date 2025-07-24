c68bb72b8b382cf7185f1ebb7868ab375f0fee7c
Aditya Joshi <joshiaditya.aj07@gmail.com>
2025-07-24 23:04:48 +0530

    refactor: bg change in the landing page

--- apps/web/src/app/(marketing)/page.tsx
+++ apps/web/src/app/(marketing)/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 
+import { GradientBars } from "@/components/gradient-bars";
 import HeroSection from "@/components/hero-section";
-import { SideWaveBars } from "@/components/sidebar-waves";
 import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function Home() {
   const isMobile = useIsMobile();
   return (
     <div>
-      <SideWaveBars
+      <GradientBars
         bars={isMobile ? 20 : 15}
-        sideWidthPercent={isMobile ? 35 : 30}
+        // sideWidthPercent={isMobile ? 35 : 30}
       />
       <HeroSection />
     </div>
